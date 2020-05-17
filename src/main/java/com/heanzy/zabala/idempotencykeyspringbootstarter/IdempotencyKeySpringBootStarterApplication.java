c80e09d939587189325a667a4bbecde73f98eb50
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-05-17 13:35:25 +0800

    Initial commit

---
+++ src/main/java/com/heanzy/zabala/idempotencykeyspringbootstarter/IdempotencyKeySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.heanzy.zabala.idempotencykeyspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotencyKeySpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotencyKeySpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heanzy/zabala/idempotencykeyspringbootstarter/IdempotencyKeySpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heanzy.zabala.idempotencykeyspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotencyKeySpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
